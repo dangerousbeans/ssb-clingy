31f416228e9b71e3d3d8d917f863f64625d5a32e
Joran <joran.k@gmail.com>
2019-07-14 11:13:27 +1200

    changed to work with rpc:connect - will now work with remote peers, not just local connections

--- index.js
+++ index.js
@@ -6,44 +6,27 @@ exports.version = '1.1.0'
 exports.manifest = {}
 
 exports.init = (api, opts) => {
-  api.connect.hook((connect, args) => {
-    const address = args[0]
-    const cb = args[1]
-    const our_id = api.whoami().id
+  api.on('rpc:connect', function (rpc, args) {
+    console.log("connection from: ", rpc.id)
 
-    connect(address, (err, x) => {
-      if (err) return cb(err)
+    var our_id = api.whoami().id
+    var connecting_id = rpc.id
 
-      const p = api.multiserver.parse(address)
-      if (!Array.isArray(p)
-          || p.length < 1
-          || p[0].length < 2
-          || !("key" in p[0][1]))
-      {
-        console.log("unexpected result when parsing address:", address, " result:", p)
-        return cb(null, x)
+    api.friends.isFollowing({source: our_id, dest: connecting_id}, (err, following) => {
+      if (following) {
+        console.log("already following:", connecting_id)
+      } else {
+       // Yay more friendssss
+        console.log("stranger! New friend :3", connecting_id)
+
+        api.publish({
+         type: "contact",
+         contact: connecting_id,
+         following: true
+       }, () => {
+          console.log("Followed", connecting_id)
+       })
       }
-
-      const connecting_id = '@' + p[0][1].key.toString('base64') + '.ed25519'
-
-      api.friends.isFollowing({source: our_id, dest: connecting_id}, (err, following) => {
-        if (following) {
-          console.log("already following:", connecting_id)
-      	  cb(null, x)
-        } else {
-      	  // Yay more friendssss
-          console.log("stranger! New friend :3", connecting_id)
-
-          api.publish({
-      		  type: "contact",
-      		  contact: connecting_id,
-      		  following: true
-      	  }, () => {
-            console.log("Followed", connecting_id)
-            cb(null, x)
-      	  })
-        }
-      })
     })
   })
 }
